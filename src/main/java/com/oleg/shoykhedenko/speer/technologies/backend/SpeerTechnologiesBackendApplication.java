f9e5b67976d1bb8c1bbef51f2e2b85093ba6534b
OlegShoykhedenko <70909936+OlegShoykhedenko@users.noreply.github.com>
2021-03-16 18:07:27 -0700

    Init commit

---
+++ src/main/java/com/oleg/shoykhedenko/speer/technologies/backend/SpeerTechnologiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.oleg.shoykhedenko.speer.technologies.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeerTechnologiesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpeerTechnologiesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oleg/shoykhedenko/speer/technologies/backend/SpeerTechnologiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleg.shoykhedenko.speer.technologies.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeerTechnologiesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
